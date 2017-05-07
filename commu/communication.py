f53bd1c27b575f14c58589bfb0f5e7a022156d7d
kayfelicities <390528655@qq.com>
2017-05-07 21:36:19 +0800

    communication module

---
+++ commu/communication.py
@@ -0,0 +1,32 @@
+'''communication'''
+
+class Communication():
+    '''communication class'''
+    def __init__(self):
+        '''init'''
+        pass
+
+    def connect_serial(self, com):
+        '''connect serial'''
+        pass
+
+    def disconnect_serial(self, com):
+        '''stop serial'''
+        pass
+
+    def connect_front_server(self):
+        '''connect_front_server'''
+        pass
+
+    def disconnect_front_server(self):
+        '''stop front_server'''
+        pass
+
+    def start_server(self):
+        '''connect server'''
+        pass
+
+    def stop_server(self):
+        '''stop server'''
+        pass
+
